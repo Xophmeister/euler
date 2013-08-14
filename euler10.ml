db1aa0790ddfcaf9c1414ec631ac9f9af60cb1a9
Christopher Harrison <github@acc.xoph.co>
2013-08-14 12:50:30 +0100

    Project Euler Problem 10 Final attempt with tail call recursive sieve and Big_ints: Still slow :(

--- euler10.ml
+++ euler10.ml
@@ -2,17 +2,18 @@
    Find the sum of all the primes below two million.                  *)
 
 (* Fixed recursive Sieve of Eratosthenes, with thanks to Sean McLaughin
-   for pointing out the tail recursion (or lack thereof) issue, plus
-   OCaml style tips (http://stackoverflow.com/a/18214988/876937)      *)
+   for pointing out the lack of tail call recursion in the range
+   function, plus OCaml code style tips.
+   
+   http://stackoverflow.com/a/18214988/876937                         *)
 
 let primes max =
-  let rec sieve = function
-    | []     -> failwith "WTF?"
-    | h :: t -> let doesntDivide x = (x mod h <> 0) in
-                let nonDivisors = List.filter doesntDivide t in
-                match nonDivisors with
-                  | [] -> [h]
-                  | nd -> (h :: sieve nd) in
+  let doesntDivideBy y x = (x mod y <> 0) in
+
+  let rec sieve found unsieved = match unsieved with
+    | []     -> found
+    | h :: t -> let nonDivisors = List.filter (doesntDivideBy h) t in
+                sieve (h :: found) nonDivisors in
 
   let rec range memo a b =
     if a > b then memo
@@ -20,8 +21,16 @@ let primes max =
 
   let p = List.rev (range [] 2 max) in
 
-  sieve p;;
+  sieve [] p;;
 
-let answer = List.fold_left (+) 0 (primes 1999999) in
-  print_int answer;
+(* I'm determined to get this to work!! The above is now my prime sieve
+   algorithm with tail call recursion and the summation now converts to
+   Big_int because 31-bits aren't enough to store the result...
+
+   Fingers crossed!                                                   *)
+
+open Big_int;;
+let bigPrimes = List.map big_int_of_int (primes 1999999) in
+let answer = List.fold_left add_big_int zero_big_int bigPrimes in
+  print_string (string_of_big_int answer);
   print_newline ();;
